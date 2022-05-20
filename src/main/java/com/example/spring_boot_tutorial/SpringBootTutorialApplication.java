b4b57d83dfa431aca7a9505c8fcf8c5070a19c28
Joshua C. Sadsad <joshsad2@gmail.com>
2022-05-19 23:59:02 -0400

    student class example done

--- src/main/java/com/example/spring_boot_tutorial/SpringBootTutorialApplication.java
+++ src/main/java/com/example/spring_boot_tutorial/SpringBootTutorialApplication.java
@@ -1,11 +1,14 @@
 package com.example.spring_boot_tutorial;
 
+import com.example.spring_boot_tutorial.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 // @SpringBootApplication
@@ -18,8 +21,16 @@ public class SpringBootTutorialApplication {
     }
 
     @GetMapping // This is now a REST endpoint
-    public List<String> hello() {
-        return List.of("Hello", "World");
+    public List<Student> hello() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Joshua",
+                        "test@example.com",
+                        LocalDate.of(1994, Month.OCTOBER, 21),
+                        28
+                )
+        );
     }
 }
 
