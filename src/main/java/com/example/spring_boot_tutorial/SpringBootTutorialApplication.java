448a2151720169455c085f9102eed1e1fdee2260
Joshua C. Sadsad <joshsad2@gmail.com>
2022-05-19 23:04:16 -0400

    Created first REST endpoint with @GetMapping

--- src/main/java/com/example/spring_boot_tutorial/SpringBootTutorialApplication.java
+++ src/main/java/com/example/spring_boot_tutorial/SpringBootTutorialApplication.java
@@ -3,15 +3,24 @@ package com.example.spring_boot_tutorial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 // @SpringBootApplication
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@RestController
 public class SpringBootTutorialApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTutorialApplication.class, args);
     }
 
+    @GetMapping // This is now a REST endpoint
+    public List<String> hello() {
+        return List.of("Hello", "World");
+    }
 }
 
 
